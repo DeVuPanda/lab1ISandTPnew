10794adb3b92dd06f50842eb3a3d69476640197f
Denys <denys.kalenychenko.student@knu.ua>
2024-03-02 15:01:48 +0200

    Finished all the edits I got in class

--- src/ArchiveMVCnew/ArchivenewInfrastructure/obj/Debug/net8.0/ArchivenewInfrastructure.AssemblyInfo.cs
+++ src/ArchiveMVCnew/ArchivenewInfrastructure/obj/Debug/net8.0/ArchivenewInfrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchivenewInfrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2821efc7e588b5901b1e72a86ef360650eced83e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c6c0be47e6645b7cf6393b360b77c3ae3f635bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchivenewInfrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchivenewInfrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
