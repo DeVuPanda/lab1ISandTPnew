cccffd4b047495d8a42c5abd09cbacf4a5082cef
Denys <denys.kalenychenko.student@knu.ua>
2024-03-08 13:40:12 +0200

    Finished the 1.4

--- src/ArchiveMVCnew/ArchivenewDomain/obj/Debug/net8.0/ArchivenewDomain.AssemblyInfo.cs
+++ src/ArchiveMVCnew/ArchivenewDomain/obj/Debug/net8.0/ArchivenewDomain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchivenewDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c6c0be47e6645b7cf6393b360b77c3ae3f635bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10794adb3b92dd06f50842eb3a3d69476640197f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchivenewDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchivenewDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
