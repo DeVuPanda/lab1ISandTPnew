866fd1faa284cb08340248481432977b6b2bf9a9
Denys <denys.kalenychenko.student@knu.ua>
2024-03-22 20:28:52 +0200

    date is taken from a function that displays today's date

--- src/ArchiveMVCnew/ArchivenewInfrastructure/obj/Debug/net8.0/ArchivenewInfrastructure.AssemblyInfo.cs
+++ src/ArchiveMVCnew/ArchivenewInfrastructure/obj/Debug/net8.0/ArchivenewInfrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchivenewInfrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c6fea6efefba29a0a3b70cf1ede2c40a7acc08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659052bd3ae9b7235441007c06b2a39bfeb96ffd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchivenewInfrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchivenewInfrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
